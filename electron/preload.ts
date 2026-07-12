d91616ce180fd7559c4f80a662d5ac4d0bbbfd8d
Fruizer <enzo715@gmail.com>
2026-07-12 10:23:08 +0800

    chore: wipe old scaffold and initialize clean electron-vite layout

--- electron/preload.ts
+++ electron/preload.ts
@@ -1,10 +1,10 @@
-import { ipcRenderer, contextBridge } from 'electron'
+import { ipcRenderer, contextBridge, IpcRendererEvent } from 'electron'
 
 // --------- Expose some API to the Renderer process ---------
 contextBridge.exposeInMainWorld('ipcRenderer', {
   on(...args: Parameters<typeof ipcRenderer.on>) {
     const [channel, listener] = args
-    return ipcRenderer.on(channel, (event, ...args) => listener(event, ...args))
+    return ipcRenderer.on(channel, (event: IpcRendererEvent, ...args: any[]) => listener(event, ...args))
   },
   off(...args: Parameters<typeof ipcRenderer.off>) {
     const [channel, ...omit] = args
@@ -18,7 +18,4 @@ contextBridge.exposeInMainWorld('ipcRenderer', {
     const [channel, ...omit] = args
     return ipcRenderer.invoke(channel, ...omit)
   },
-
-  // You can expose other APTs you need here.
-  // ...
 })
